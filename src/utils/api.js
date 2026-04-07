6f39290eaa8c6f4a24d44c1dad19d096eceadff4
Feedback Form Developer <feedback-form@github.com>
2026-04-07 18:38:19 +0530

    Update frontend to use correct Railway backend URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 const getToken = () => localStorage.getItem('token');
 
